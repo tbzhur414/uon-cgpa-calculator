dd85f36534ac35abeb42347511939bbd00598aed
Hur Abbas <tbzgrouphur@gmail.com>
2025-09-09 16:36:02 +0500

    Initial commit

---
+++ app/src/main/java/com/example/uoncgpacalculator/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.uoncgpacalculator
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide() // Safe call for nullable ActionBar
+
+        val webView: WebView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://tbzhur414.github.io/cgpa-calculator/")
+    }
+
+    override fun onBackPressed() {
+        val webView: WebView = findViewById(R.id.webView)
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
